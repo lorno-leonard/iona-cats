4a3b4d37d88be23f0c82420ed17859010cadb590
Leonard Laput <online.lorno@gmail.com>
2021-02-07 00:52:32 +0800

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+// import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import './App.scss';
+
+const App: FC = () => {
+  return (
+    <h1>Test</h1>
+  )
+}
+
+export default App;
